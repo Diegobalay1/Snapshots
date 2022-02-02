5346c3004d17104c51b18fdd26672bf3d344d808
balay <dlolhd@gmail.com>
2022-02-02 09:38:00 +0100

    Commit de Firebase Authentication: Mejoras.

--- app/src/main/java/com/diego/kotlin/snapshots/MainActivity.kt
+++ app/src/main/java/com/diego/kotlin/snapshots/MainActivity.kt
@@ -5,11 +5,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.diego.kotlin.snapshots.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import java.util.*
 
@@ -55,8 +57,12 @@ class MainActivity : AppCompatActivity() {
         getContent.launch(intentLogin)
     }
     private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == Activity.RESULT_OK) {
-
+        if (it.resultCode == RESULT_OK) {
+            Toast.makeText(this, "Bienvenido...", Toast.LENGTH_SHORT).show()
+        } else {
+            if (IdpResponse.fromResultIntent(it.data) == null) {//en caso de pulsación del botón de retroceso
+                finish()
+            }
         }
     }
 
